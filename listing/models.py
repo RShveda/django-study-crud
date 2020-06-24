f7841ab6f6b72517eaa344dde4580d1f679058f6
RShveda <rshveda@gmail.com>
2020-06-24 16:17:13 +0300

    added fuunctionality to Create, Edit, and Delete Student

--- listing/models.py
+++ listing/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class School(models.Model):
     name = models.CharField(max_length=40)
     location = models.CharField(max_length=40)
     students_counter = models.IntegerField(default = 0)
+
+    def get_absolute_url(self):
+        return reverse('listing:school', kwargs={'school': self.name})
 
     def __str__(self):
         return self.name
@@ -19,6 +23,16 @@ class Student(models.Model):
         a.students_counter = len(Student.objects.filter(school = a)) + 1
         a.save()
         super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        ## increment students counter for this particular school
+        a = self.school
+        a.students_counter = len(Student.objects.filter(school = a)) - 1
+        a.save()
+        super().delete(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('listing:student', kwargs={"school": self.school.name, "student": self.pk})
 
     def __str__(self):
         return self.name
--- listing/views.py
+++ listing/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from listing.models import School, Student
 from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+
 # Create your views here.
 
 class SchoolList(ListView):
@@ -12,15 +15,39 @@ class SchoolStudentList(ListView):
     template_name="listing/students.html"
 
     def get_queryset(self):
-        self.school = get_object_or_404(School, name = self.kwargs['school'])
-        return Student.objects.filter(school = self.school)
+        self.school_name = get_object_or_404(School, name = self.kwargs['school'])
+        return Student.objects.filter(school = self.school_name)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['school'] = self.school
+        context['school_name'] = self.school_name
         return context
 
 class StudentDetailView(DetailView):
     model = Student
-    template_name="listing/student.html"
+    fields = ["name", "school"]
+    template_name = "listing/student.html"
     pk_url_kwarg = "student"
+
+class StudentCreateView(CreateView):
+    model = Student
+    fields = ["name"]
+
+    def form_valid(self, form):
+        form.instance.school = get_object_or_404(School, name = self.kwargs['school'])
+        return super().form_valid(form)
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    fields = ["name", "school"]
+    pk_url_kwarg = "student"
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    pk_url_kwarg = "student"
+    def get_success_url(self, **kwargs):
+        return reverse_lazy("listing:school", kwargs={"school":self.object.school})
+
+class SchoolCreateView(CreateView):
+    model = School
+    fields = ["name", "location"]
